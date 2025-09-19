12b100563ba08695c4ef4bd1ff9d794e7a1f4e3a
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-09-19 19:15:26 +0600

    react commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Countries></Countries>
+      </div>
+    </>
+  )
+}
+
+export default App
